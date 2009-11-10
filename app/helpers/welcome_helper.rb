c2dc1ce4ed288d95c2184394b2003b391ec11f60
Larry Kooper <lkooper@recyclebank.com>
2009-11-10 14:52:30 -0500

    changed time formatting

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,17 +1,17 @@
 module WelcomeHelper 
   
   def earlier
-    SystemInfo.earlier.strftime("%I:%M %p %m/%d/%y")    
+    SystemInfo.earlier.strftime("%l:%M %p %m/%d/%y")    
   end
   
   def flu 
     si = SystemInfo.find(1)
-    si.followers_last_update.blank? ? "never" : si.followers_last_update.strftime("%I:%M %p %m/%d/%y")
+    si.followers_last_update.blank? ? "never" : si.followers_last_update.strftime("%l:%M %p %m/%d/%y")
   end 
   
   def iflu
     si = SystemInfo.find(1)
-    si.i_follow_last_update.strftime("%I:%M %p %m/%d/%y")
+    si.i_follow_last_update.strftime("%l:%M %p %m/%d/%y")
   end 
   
   def yes_no(value)
