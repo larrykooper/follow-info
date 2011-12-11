f0b55c586237ae03388ac02141512b079472acf6
Larry Kooper <larry.kooper@gmail.com>
2011-12-10 19:21:35 -0500

    fixed logout bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ FollowInfo::Application.routes.draw do
 
   root :to => "welcome#index" 
   match '/' => 'welcome#index'
+  
+  devise_scope :follow_info_user do
+    match '/follow_info_users/sign_out' => "devise/sessions#destroy"
+  end
   resources :users
   match ':controller/:action'
   match ':controller/:action/:id'
