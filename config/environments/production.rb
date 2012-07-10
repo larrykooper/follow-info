5728459effd170c9403c807cecb6d3c8af736c39
Larry Kooper <larry.kooper@gmail.com>
2012-07-09 23:21:28 -0400

    Precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,10 @@ FollowInfo::Application.configure do
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( base.css screen.css)
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
