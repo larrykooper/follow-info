8da73a602b0a44171e6d96f845f74a41b520d638
Larry Kooper <larry.kooper@gmail.com>
2012-07-09 23:17:41 -0400

    Config changes

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,14 @@ load(File.expand_path('../heroku_env.rb', __FILE__))
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+#Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module FollowInfo
   class Application < Rails::Application
@@ -38,7 +45,10 @@ module FollowInfo
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]    
+    config.filter_parameters += [:password]
+    
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true    
 
     # Enable the asset pipeline
     config.assets.enabled = true
