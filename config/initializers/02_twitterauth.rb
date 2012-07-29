8b6f12882085f2d4bc55ac35e765bd369e9585b0
Larry Kooper <larry.kooper@gmail.com>
2012-07-29 00:38:18 -0400

    Check in initializer

---
+++ config/initializers/02_twitterauth.rb
@@ -0,0 +1,9 @@
+require "twitcon"
+
+Twitterauth::CLIENT = Twitcon::Client.new(
+  :consumer_key => ENV["TWITTER_CONSUMER_KEY"],
+  :consumer_secret => ENV["TWITTER_CONSUMER_SECRET"],
+  :oauth_token => ENV["TWITTER_OAUTH_TOKEN"],
+  :oauth_token_secret => ENV["TWITTER_OAUTH_TOKEN_SECRET"] 
+)
+  
