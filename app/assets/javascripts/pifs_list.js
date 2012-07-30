23f7e9ef6326d8b4428c76bf302730a614bd52ed
Larry Kooper <larry.kooper@gmail.com>
2012-07-29 21:45:16 -0400

    Changed sorting to descending for I Follow Nbr

--- app/assets/javascripts/pifs_list.js
+++ app/assets/javascripts/pifs_list.js
@@ -1,7 +1,7 @@
 $(document).ready(function() 
     { 
-        $("#myPifs").tablesorter({headers: { 0: { sorter: false}, 1: {sorter: false},
-	2: {sorter: true}, 3: {sorter: false}, 4: {sorter: true}, 5: {sorter: false} }}); 
+        $("#myPifs").tablesorter({headers: { 0: { sorter: true}, 1: {sorter: false},
+	2: {sorter: true}, 3: {sorter: false}, 4: {sorter: true}, 5: {sorter: false} }, sortInitialOrder: "desc"}); 
     } 
 ); 
    
