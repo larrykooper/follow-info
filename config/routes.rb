174a4ef199b0bce0cdbb21bc60d63723cd218805
Larry Kooper <larry.kooper@gmail.com>
2012-07-30 23:39:57 -0400

    Deleted legacy routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,22 @@ FollowInfo::Application.routes.draw do
   root :to => "welcome#index" 
   match '/' => 'welcome#index'
   match "tags" => "tag#list"
+  get "/welcome/index"
+  get "/welcome/list_pif"
+  get "/welcome/list_stats"
+  get "/welcome/list_follers"
+  get "/welcome/list_unfollowed"
+  get "/welcome/list_idropped"
+  get "/tag/edit"
+  post "/welcome/update_follers"
+  post "/welcome/update_pif"
   
   devise_scope :follow_info_user do
     match '/follow_info_users/sign_out' => "devise/sessions#destroy"
   end
   resources :users
-  match ':controller/:action'
-  match ':controller/:action/:id'
-  match ':controller/:action/:id.:format'
+  
+  #match ':controller/:action'
+  #match ':controller/:action/:id'
+  #match ':controller/:action/:id.:format'
 end
