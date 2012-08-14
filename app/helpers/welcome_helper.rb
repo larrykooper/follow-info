88cfcf4d4bdd87fbde80ac10dcf845835b305d35
Larry Kooper <larry.kooper@gmail.com>
2012-08-14 11:35:52 -0400

    Switched tag display in PIFs to use new schema. Also some space removal.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,54 +1,66 @@
-module WelcomeHelper 
-  
+module WelcomeHelper
+
   def earlier
-    SystemInfo.earlier.strftime("%l:%M %p %m/%d/%y")    
+    SystemInfo.earlier.strftime("%l:%M %p %m/%d/%y")
   end
-  
-  def flu 
+
+  def flu
     si = SystemInfo.find(1)
     si.followers_last_update.blank? ? "never" : si.followers_last_update.strftime("%l:%M %p %m/%d/%y")
-  end 
-  
+  end
+
   def iflu
     si = SystemInfo.find(1)
     si.i_follow_last_update.strftime("%l:%M %p %m/%d/%y")
-  end 
-  
+  end
+
   def yes_no(value)
     value ? 'yes' : 'no'
   end
-  
+
   def display_tags(taggings)
     if taggings.empty?
       "no"
     else
       yes_no(taggings.first.tag.is_published)
-    end  
+    end
   end
-  
-  def all_my_tags 
+
+  def all_my_tags
     taglist = ""
     Tag.used_tags.each do |tag|
       taglist << "<span class='tagName"
-      taglist << " published" if tag.is_published 
+      taglist << " published" if tag.is_published
       taglist << "'>"
-      taglist << tag.name 
+      taglist << tag.name
       taglist << "</span>"
     end
     taglist.html_safe
-  end 
-  
+  end
+
+  def tag_display_for_current_user(tag)
+    fiut = FollowInfoUsersTag.find_by_follow_info_user_id_and_tag_id(current_follow_info_user.id, tag.id)
+    output = ""
+    output << "<span"
+    output << " class='published'" if fiut.is_published
+    output << ">"
+    output << tag.name
+    output << "</span>"
+    output.html_safe
+  end
+
+
   def tags_display_for(tag_arr)
     taglist = ""
     tag_arr.each do |tag|
       taglist << "<span"
-      taglist << " class='published'" if tag.is_published 
+      taglist << " class='published'" if tag.is_published
       taglist << ">"
       taglist << tag.name
       taglist << "</span>"
       taglist << ", " unless tag_arr.last == tag
-    end 
-    taglist.html_safe     
+    end
+    taglist.html_safe
   end
-  
+
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,85 +1,90 @@
 # A twitter_user is one Twitter account.
-# It could be somebody I follow or someone who follows me, 
+# It could be somebody followed by one or more FI users, or someone who follows one or more FI users,
 # or both.
 
-class TwitterUser < ActiveRecord::Base 
+class TwitterUser < ActiveRecord::Base
   attr_accessible :name, :nbr_followers, :follows_me, :i_follow, :i_follow_nbr, :follows_me_nbr, :taken_care_of, :last_time_tweeted
-  has_many :taggings, :dependent => :destroy   
-  has_many :tags, :through => :taggings
-  
-  require 'math_stuff' 
+  has_many :followings
+  # has_many :taggings, :dependent => :destroy
+  # has_many :tags, :through => :taggings
 
-  # CLASS METHODS   
-  def self.create_new_foller(foller, ind)     
+  require 'math_stuff'
+
+  # CLASS METHODS
+  def self.create_new_foller(foller, ind)
      twitter_user = TwitterUser.new({:name => foller.screen_name,
       :nbr_followers => foller.followers_count,
       :follows_me => true,
       :i_follow => false,
-      :follows_me_nbr => ind, 
-      :taken_care_of => true})               
-    twitter_user.save!       
-  end 
-  
-  # Add a new person I follow from the Twitter API 
+      :follows_me_nbr => ind,
+      :taken_care_of => true})
+    twitter_user.save!
+  end
+
+  # Add a new person I follow from the Twitter API
   def self.create_new_pif(pif, ind)
     last_time_tweeted = pif.status.nil? ? nil : pif.status['created_at']
     twitter_user = TwitterUser.new({:name => pif.screen_name,
-      :nbr_followers => pif.followers_count, 
+      :nbr_followers => pif.followers_count,
       :last_time_tweeted => last_time_tweeted,
       :follows_me => false,
       :i_follow => true,
-      :i_follow_nbr => ind, 
-      :taken_care_of => true})               
-    twitter_user.save! 
-  end  
-  
-  def self.pifs_deleted 
+      :i_follow_nbr => ind,
+      :taken_care_of => true})
+    twitter_user.save!
+  end
+
+  def self.pifs_deleted
     TwitterUser.where("taken_care_of = false AND i_follow = true")
-  end 
-  
-  def self.followers_deleted 
+  end
+
+  def self.followers_deleted
    TwitterUser.where("taken_care_of = false AND follows_me = true")
-  end  
-  
-  # PUBLIC INSTANCE METHODS 
-  
- def process_foller(foller, ind)    
+  end
+
+  # PUBLIC INSTANCE METHODS
+
+ def process_foller(foller, ind)
     # Update one twitter_user who follows me
     unless self.follows_me
-      self.follows_me = true        
-    end 
-    self.nbr_followers = foller.followers_count 
-    self.follows_me_nbr = ind 
-    self.taken_care_of = true    
-    self.save!          
-  end    
-  
-  def process_pif(pif, ind) 
-    last_time_tweeted = pif.status.nil? ? nil : pif.status['created_at']
-    # Update one twitter_user that I follow      
-    unless self.i_follow   
-      self.i_follow = true        
-    end     
-    self.nbr_followers = pif.followers_count
-    self.last_time_tweeted = last_time_tweeted if last_time_tweeted 
-    self.i_follow_nbr = ind 
+      self.follows_me = true
+    end
+    self.nbr_followers = foller.followers_count
+    self.follows_me_nbr = ind
     self.taken_care_of = true
-    self.save!   
+    self.save!
+  end
+
+  def process_pif(pif, ind)
+    last_time_tweeted = pif.status.nil? ? nil : pif.status['created_at']
+    # Update one twitter_user that I follow
+    unless self.i_follow
+      self.i_follow = true
+    end
+    self.nbr_followers = pif.followers_count
+    self.last_time_tweeted = last_time_tweeted if last_time_tweeted
+    self.i_follow_nbr = ind
+    self.taken_care_of = true
+    self.save!
     # invalidate cache
-    ActionController::Base.new.expire_fragment("twitter_user-#{self.id}")                   
-  end      
-  
+    ActionController::Base.new.expire_fragment("twitter_user-#{self.id}")
+  end
+
+  def tag(fiu)
+    followings.where(:follow_info_user_id => fiu.id).first.tag
+  end
+
   def tag_list
-     (tags.collect {|tag| tag.name }).join(", ") 
-  end   
- 
+     (tags.collect {|tag| tag.name }).join(", ")
+  end
+
   def tag_with_manually(list)
     Tag.transaction do
       taggings.destroy_all
-      Tag.parse(list).each do |name|        
-        Tag.find_or_create_by_name(name).add_twitter_user_manually(self)        
+      Tag.parse(list).each do |name|
+        Tag.find_or_create_by_name(name).add_twitter_user_manually(self)
       end
-    end   
-  end  
-    
-end 
+    end
+  end
+
+end
