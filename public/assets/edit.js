e93aa3bcb5a88789e24e288d5c8d5bca587488dd
Larry Kooper <larry.kooper@gmail.com>
2012-08-14 14:09:14 -0400

    Assets

--- public/assets/edit.js
+++ public/assets/edit.js
@@ -1,6 +1,12 @@
 $(document).ready(function() 
   { 
     $(".tagName").click(function(event) {
-		  $('#tags').val($(event.target).text());	
+      var thetag = $(event.target);
+		  $('#tags').val(thetag.text());
+		  if (thetag.hasClass('published')) {
+		    $('#is_tag_pub').html('yes');
+		  } else {
+		     $('#is_tag_pub').html('no');
+		  }   
     });
 });
