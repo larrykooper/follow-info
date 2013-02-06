8c1ef5c50613a9741819e41736e6ecdd18ac73f4
Larry Kooper <larry.kooper@gmail.com>
2013-02-05 20:08:28 -0500

    bug fix for tagging observer

--- app/models/tagging_observer.rb
+++ app/models/tagging_observer.rb
@@ -1,5 +1,5 @@
 class TaggingObserver < ActiveRecord::Observer 
-  def after_update(a_tagging)  
+  def after_save(a_tagging) 
     ActionController::Base.new.expire_fragment("user-#{a_tagging.user.id}")
   end
 
