6bd8c297c0c687740303196283287b404e9f0d83
Larry Kooper <larry.kooper@gmail.com>
2013-02-05 19:35:32 -0500

    fixed bug in user_observer

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,5 @@
 class UserObserver < ActiveRecord::Observer 
   def after_update(a_user)
-    expire_fragment("user-#{a_user.id}") 
+    ActionController::Base.new.expire_fragment("user-#{a_user.id}") 
   end 
 end
