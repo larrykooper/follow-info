6e5afef16112056f0dabc49cd2b723e8502266b6
larrykooper <larry.kooper@gmail.com>
2015-12-20 10:05:35 -0500

    Updated to add key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -192,6 +192,7 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
+  config.secret_key = ENV["DEVISE_SECRET_KEY"] 
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
