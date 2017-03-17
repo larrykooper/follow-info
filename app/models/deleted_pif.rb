818ce142bf82b47214543f9e8ad36fd19a5e3101
larrykooper <larry.kooper@gmail.com>
2017-03-16 22:06:55 -0400

    Code cleanup

--- app/models/deleted_pif.rb
+++ app/models/deleted_pif.rb
@@ -1,14 +1,11 @@
 # A Deleted Pif is a user that I used to follow who I either unfollowed, or their account was cancelled
 # The deleted ones are kept in this table until the next update of the PIF list
 
-
-
 class DeletedPif < ActiveRecord::Base
   belongs_to :follow_info_user
 
   def clear_out_for(follow_info_user)
     DeletedPif.where(:follow_info_user_id => follow_info_user.id).destroy_all
   end
-
 
 end
