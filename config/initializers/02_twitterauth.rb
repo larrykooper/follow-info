6dd56b499f22e080523d0372fb4e7c704ae31bb3
larrykooper <larry.kooper@gmail.com>
2018-02-06 22:47:49 -0500

    deprecating file

--- config/initializers/02_twitterauth.rb
+++ config/initializers/02_twitterauth.rb
@@ -1,9 +1,8 @@
-require "twitcon"
+require "twitter"
 
-Twitterauth::CLIENT = Twitcon::Client.new(
-  :consumer_key => ENV["TWITTER_CONSUMER_KEY"],
-  :consumer_secret => ENV["TWITTER_CONSUMER_SECRET"],
-  :oauth_token => ENV["TWITTER_OAUTH_TOKEN"],
-  :oauth_token_secret => ENV["TWITTER_OAUTH_TOKEN_SECRET"] 
-)
-  
+# Twitterauth::CLIENT = Twitter::Client.new(
+#   :consumer_key => ENV["TWITTER_CONSUMER_KEY"],
+#   :consumer_secret => ENV["TWITTER_CONSUMER_SECRET"],
+#   :oauth_token => ENV["TWITTER_OAUTH_TOKEN"],
+#   :oauth_token_secret => ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+# )
