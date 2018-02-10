4026528e408fe64547ca4cd4da4980022af16f19
larrykooper <larry.kooper@gmail.com>
2018-02-09 22:49:16 -0500

    updated assets

--- public/assets/edit.js
+++ public/assets/edit.js
@@ -1,12 +1,6 @@
 $(document).ready(function() 
   { 
     $(".tagName").click(function(event) {
-      var thetag = $(event.target);
-		  $('#tag').val(thetag.text());
-		  if (thetag.hasClass('published')) {
-		    $('#is_tag_pub').html('yes');
-		  } else {
-		     $('#is_tag_pub').html('no');
-		  }
+		  $('#tags').val($(event.target).text());	
     });
 });
