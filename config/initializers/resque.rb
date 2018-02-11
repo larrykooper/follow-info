b5bdbf2f035b59ff642a0d24d9e9440e479ddd73
larrykooper <larry.kooper@gmail.com>
2018-02-10 19:35:52 -0500

    updated resque initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,14 @@ rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+
+if Rails.env.production?
+  uri = URI.parse ENV["REDISTOGO_URL"]
+  Resque.redis = Redis.new(host: uri.host, port: uri.port,
+                           password: uri.password)
+else
+  Resque.redis = "localhost:6379"
+end
+
+#Resque.redis = resque_config[rails_env]  # problem
 Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60) # 24hrs in seconds
