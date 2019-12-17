14ef709c8782bbfb3f7fb808a1297966da80d31d
larrykooper <larry.kooper@gmail.com>
2019-12-16 20:08:21 -0500

    fixed a test

--- test/models/system_info_test.rb
+++ test/models/system_info_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "earlier returns the earlier last update" do
     six_days_ago = 6.days.ago
-    si = SystemInfo.create(followers_last_update: six_days_ago, i_follow_last_update: 3.days.ago, id: 1)
+    si = SystemInfo.update(followers_last_update: six_days_ago, i_follow_last_update: 3.days.ago)
     assert_equal(six_days_ago.to_s, SystemInfo.earlier.to_s)
 
   end
